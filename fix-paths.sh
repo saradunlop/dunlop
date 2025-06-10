6a48142283eb59a93e81521d3b0f864486d2b3e2
Joe Wheatley <joe@example.com>
2025-06-10 17:26:31 +0100

    Fix: Update all paths to be relative

---
+++ fix-paths.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Fix paths in commercial pages
+find commercial -name "*.html" -type f -exec sed -i '' 's|href="/commercial/|href="./|g' {} +
+find commercial -name "*.html" -type f -exec sed -i '' 's|href="/narrative/|href="../narrative/|g' {} +
+find commercial -name "*.html" -type f -exec sed -i '' 's|href="/info/|href="../info/|g' {} +
+find commercial -name "*.html" -type f -exec sed -i '' 's|href="/index.html|href="../index.html|g' {} +
+find commercial -name "*.html" -type f -exec sed -i '' 's|src="/images/|src="../images/|g' {} +
+
+# Fix paths in narrative pages
+find narrative -name "*.html" -type f -exec sed -i '' 's|href="/narrative/|href="./|g' {} +
+find narrative -name "*.html" -type f -exec sed -i '' 's|href="/commercial/|href="../commercial/|g' {} +
+find narrative -name "*.html" -type f -exec sed -i '' 's|href="/info/|href="../info/|g' {} +
+find narrative -name "*.html" -type f -exec sed -i '' 's|href="/index.html|href="../index.html|g' {} +
+find narrative -name "*.html" -type f -exec sed -i '' 's|src="/images/|src="../images/|g' {} +
+
+# Fix paths in info pages
+find info -name "*.html" -type f -exec sed -i '' 's|href="/info/|href="./|g' {} +
+find info -name "*.html" -type f -exec sed -i '' 's|href="/commercial/|href="../commercial/|g' {} +
+find info -name "*.html" -type f -exec sed -i '' 's|href="/narrative/|href="../narrative/|g' {} +
+find info -name "*.html" -type f -exec sed -i '' 's|href="/index.html|href="../index.html|g' {} +
+find info -name "*.html" -type f -exec sed -i '' 's|src="/images/|src="../images/|g' {} +
+
+# Fix paths in root index.html
+sed -i '' 's|href="/commercial/|href="./commercial/|g' index.html
+sed -i '' 's|href="/narrative/|href="./narrative/|g' index.html
+sed -i '' 's|href="/info/|href="./info/|g' index.html
+sed -i '' 's|src="/images/|src="./images/|g' index.html 
